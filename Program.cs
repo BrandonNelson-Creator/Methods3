88943d3184c119f25b81c01a0948db0d768d3c95
BrandonNelson-Creator <n.ellyson@hotmail.com>
2021-09-27 13:40:02 -0300

    simple loops

--- Program.cs
+++ Program.cs
@@ -8,9 +8,18 @@ namespace Methods3
 {
     class Program
     {
+        static int count = 0;
+        static void Loop()
+
+        {
+            count = count + 1;
+            Console.WriteLine("This is Loop # " + count);
+            Loop();
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Methods3");
+            Loop();
             Console.ReadKey(true);
         }
     }
